f7f8f1fbf6d7eff106c23017e7492e4ce9e1d764
shahabasict <shahabasict@gmail.com>
2024-09-21 21:41:28 +0530

    GateWay Implemented

---
+++ Backend/GateWay/src/main/java/fitwell_project/com/GateWay/GateWayApplication.java
@@ -0,0 +1,13 @@
+package fitwell_project.com.GateWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayApplication.class, args);
+	}
+
+}
